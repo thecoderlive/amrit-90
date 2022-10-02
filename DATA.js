13454269fae52f81591cfe75fbe9435dfb94099a
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-03 00:57:54 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "308d4e4f-b1d5-4833-a2ff-8b6100b960f6",
+    "home_feed": [
+        {
+            "id": "4af3e820-ce1f-494b-a1ba-1ed4c2155925",
+            "artist_name": "accept",
+            "home_carousel": [
+                {
+                    "id": "ac6601a8-cbff-45c7-93cd-698362cdcb3c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/188.png"
+                },
+                {
+                    "id": "624ec793-b7dd-4896-a7d0-73ce9a37ed55",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/353.png"
+                },
+                {
+                    "id": "0dfb0891-32b8-4dd9-a6f0-c6a0c7078107",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/276.png"
+                },
+                {
+                    "id": "ec6965d1-1c36-4390-ace0-d781487257c9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/348.png"
+                },
+                {
+                    "id": "a5dc00d8-933f-4f61-acf1-0a361915d8c7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/209.png"
+                }
+            ],
+            "art_caption": "police",
+            "art_description": "Rock pattern house assume. Rate place major alone skill. Bag trade south."
+        },
+        {
+            "id": "171063fa-d4e2-48d7-9a6d-c3a4b25cf40a",
+            "artist_name": "act",
+            "home_carousel": [
+                {
+                    "id": "ef92ac77-a45b-4ee5-842e-a1e48bf23ec1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/221.png"
+                },
+                {
+                    "id": "0e94d538-dfd8-449c-9e7d-45c4000107c0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/38.png"
+                },
+                {
+                    "id": "4905ddf4-4901-4c43-8618-4690e1aecf6e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/119.png"
+                },
+                {
+                    "id": "e129fd81-680c-4442-b10e-78dec473bcb1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png"
+                },
+                {
+                    "id": "33d4327a-5dc8-4f90-9b23-936af427b4f4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/158.png"
+                }
+            ],
+            "art_caption": "option",
+            "art_description": "Act interview drug. Wrong read able subject case."
+        },
+        {
+            "id": "ec646ce5-88b1-4605-b214-82505aa4a1a0",
+            "artist_name": "note",
+            "home_carousel": [
+                {
+                    "id": "671d8745-031a-47c1-bd88-21853d0acffa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                },
+                {
+                    "id": "ff11dc25-7a79-493f-9f92-7f613134e70b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/115.png"
+                },
+                {
+                    "id": "0a6fc1d9-824f-43b8-afa2-c543967258b6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/149.png"
+                },
+                {
+                    "id": "0ada9db7-f70d-432d-8cee-87b94c540c6c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/114.png"
+                },
+                {
+                    "id": "12fa67ef-b499-43ba-bf8c-30ac40bc9a54",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/228.png"
+                }
+            ],
+            "art_caption": "option",
+            "art_description": "Manager future western structure and. Read wife middle create right upon."
+        },
+        {
+            "id": "036eaa90-2c27-4aea-a9fc-4685e6dcde5d",
+            "artist_name": "mention",
+            "home_carousel": [
+                {
+                    "id": "4bb7858d-3dfe-4619-8a84-f8be2587063c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/127.png"
+                },
+                {
+                    "id": "7338188d-93c6-4889-9204-181c313165db",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png"
+                },
+                {
+                    "id": "a4fe9cb7-3719-4c12-8e0b-b027203d3460",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/160.png"
+                },
+                {
+                    "id": "5c36c015-6347-4b74-8ee7-7ef05f4724b8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/89.png"
+                },
+                {
+                    "id": "6a366889-359d-4f57-813d-2b2654b5d2ac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/326.png"
+                }
+            ],
+            "art_caption": "special",
+            "art_description": "Rock pattern house assume. Rate place major alone skill. Bag trade south."
+        },
+        {
+            "id": "e9ab1ee7-9917-4334-ab53-cc2444c8313e",
+            "artist_name": "model",
+            "home_carousel": [
+                {
+                    "id": "87c9a566-ec79-4192-a0e2-eef3df536b29",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/243.png"
+                },
+                {
+                    "id": "2239fd11-663f-4eb6-995c-6a8a0b67d651",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/87.png"
+                },
+                {
+                    "id": "89c77318-9b58-4c35-8d3d-a577dd9cb604",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/99.png"
+                },
+                {
+                    "id": "6b7d2a90-bb80-4aa0-b146-d4618229d71f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/20.png"
+                },
+                {
+                    "id": "df0bd3ce-c47c-439d-8f02-a770a1c4fe11",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/25.png"
+                }
+            ],
+            "art_caption": "he",
+            "art_description": "Skill option once one. Order year if goal of guy save."
+        }
+    ]
+}
